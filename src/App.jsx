996962ba2a2a210131244e720a6ad6b327e796df
rshihab21 <shihabcse21@gmail.com>
2024-08-16 20:09:32 +0600

    Implement footer section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from "./Components/About/About";
 import Services from "./Components/Services/Services";
 import Mywork from "./Components/Mywork/Mywork";
 import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Services />
       <Mywork />
       <Contact />
+      <Footer />
     </div>
   );
 };
